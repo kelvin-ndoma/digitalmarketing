151d26a8fa21028d0bf436029425f604bc94e889
kelvin-ndoma <kelvinndomamutua@gmail.com>
2023-09-16 12:51:50 +0300

    afew basics

--- api/server.js
+++ api/server.js
@@ -1,19 +1,34 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-// Add this before server.use(router)
+server.use(middlewares);
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  '/api/*': '/$1',
+  '/blog/:resource/:id/show': '/:resource/:id',
+}));
 
-// Export the Server API
-module.exports = server
+server.use(jsonServer.bodyParser);
+
+// Custom route for adding a new user to the /subscribers endpoint
+server.post('/subscribers', (req, res) => {
+  const { name, email, id } = req.body;
+  if (!name || !email || !id) {
+    res.status(400).json({ error: 'Name, email, and ID are required' });
+  } else {
+    const db = router.db;
+    db.get('subscribers')
+      .push({ name, email, id })
+      .write();
+    res.status(201).json({ message: 'User added successfully' });
+  }
+});
+
+server.use(router);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});

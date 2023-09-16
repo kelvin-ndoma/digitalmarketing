20441e18b7692838a0ee804f5673869754d576c6
kelvin-ndoma <kelvinndomamutua@gmail.com>
2023-09-16 13:40:59 +0300

    a new adjust

--- api/server.js
+++ api/server.js
@@ -1,14 +1,16 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('db.json'); // Replace 'db.json' with your data file
 const middlewares = jsonServer.defaults();
 
-server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-  '/blog/:resource/:id/show': '/:resource/:id',
-}));
+// Enable CORS for all routes (configure as needed)
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
+server.use(middlewares);
 server.use(jsonServer.bodyParser);
 
 // Custom route for adding a new user to the /subscribers endpoint
